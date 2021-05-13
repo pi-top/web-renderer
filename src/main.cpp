2a1b4c4c6c9d49ddaa8fa454655bf245acd731df
Jorge Capona <jcapona@users.noreply.github.com>
2021-05-13 14:13:42 -0400

    Support command line arguments to customize application (#22)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include <QGuiApplication>
+#include <QCommandLineOption>
 #include <QCommandLineParser>
+#include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QScreen>
 #include <QThread>
 #include <QtWebEngine>
+#include <QUrl>
 
 #include "config.h"
 #include "console_log_handler.h"
@@ -82,8 +84,119 @@ int runCommand(const QString &command, const QStringList &args, int timeout,
   return exitCode;
 }
 
+void waitForServerResponse(const QUrl url)
+{
+  qInfo() << "Waiting for backend web server response...";
+  bool serverIsUp = false;
+  int counter = 0;
+  int counterMax = 30;
+  while (serverIsUp == false)
+  {
+    if (counter >= counterMax)
+    {
+      qCritical("Unable to contact web server!");
+      exit(1);
+    }
+
+    QString resp;
+    int exitCode = runCommand("curl",
+                              QStringList() << "--max-time"
+                                            << "1"
+                                            << "--silent"
+                                            << "--fail"
+                                            << "--output"
+                                            << "/dev/null" << url.toString(),
+                              1000, resp);
+
+    qDebug() << exitCode;
+
+    if (exitCode == 0)
+    {
+      qInfo() << "Backend web server responded!";
+      break;
+    }
+
+    qInfo() << "Backend web server did not respond - sleeping for 1 second...";
+    QThread::sleep(1);
+    ++counter;
+  }
+}
+
+void parseSizeArgument(QString sizeStr, float &width, float &height)
+{
+  QRegExp rx("x");
+  QStringList sizeList = sizeStr.split(rx, QString::SkipEmptyParts);
+
+  if (sizeList.size() != 2)
+  {
+    qCritical("Error: Invalid size argument. Make sure it's in the format '<width_scalar>x<height_scalar>' where each scalar is a decimal number from 0 to 1 (e.g.: '0.6x0.4').");
+    exit(1);
+  }
+
+  width = sizeList.at(0).toFloat();
+  height = sizeList.at(1).toFloat();
+  if (width <= 0.0 || width > 1.0 || height <= 0.0 || height > 1.0)
+  {
+    qCritical("Error: size scalars must be a decimal number from 0 to 1 (e.g.: '0.6x0.4').");
+    exit(1);
+  }
+}
+
 int main(int argc, char *argv[])
 {
+
+  QGuiApplication app(argc, argv);
+  QtWebEngine::initialize();
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription("Application that presents web content in a way that looks like a native application window");
+
+  parser.addPositionalArgument("url", "URL to open");
+
+  QCommandLineOption kioskModeOption(QStringList() << "k" << "kiosk", "Start in kiosk mode. In this mode, the application will start in fullscreen with minimal UI. It will prevent the user from quitting or performing any interaction outside of usage of the application");
+  parser.addOption(kioskModeOption);
+  QCommandLineOption sizeOption(QStringList() << "s" << "size", "Window size relative to screen, expressed as '<width_scalar>x<height_scalar>' (e.g. '0.6x0.4'), where each scalar is a decimal number from 0 to 1.", "size", "0.65x0.55");
+  parser.addOption(sizeOption);
+  QCommandLineOption titleOption(QStringList() << "t" << "window-title", "Specify a title for the window. This will appear in the title bar.", "title", "web-renderer");
+  parser.addOption(titleOption);
+  QCommandLineOption iconOption(QStringList() << "i" << "icon", "Specify the path to an icon to be set as the application icon. This will appear in the title bar (unless the '--hide-frame' option is selected), and in the application bar.", "path", "");
+  parser.addOption(iconOption);
+  QCommandLineOption hideWindowFrameOption(QStringList() << "hide-frame", "Hide the frame (title bar and border) of the window. This means there will be no 'close', 'minimize' and 'maximize' buttons. Without this, the user cannot move or resize the window via the window system.");
+  parser.addOption(hideWindowFrameOption);
+  QCommandLineOption helpOption = parser.addHelpOption();
+
+  parser.process(app);
+
+  if (parser.isSet(helpOption))
+  {
+    parser.showHelp(0);
+    return 0;
+  }
+
+  bool kioskModeArg = parser.isSet(kioskModeOption);
+  bool hideWindowFrameArg = parser.isSet(hideWindowFrameOption);
+
+  QString sizeStr = parser.value(sizeOption);
+  float width, height;
+  parseSizeArgument(sizeStr, width, height);
+
+  const QString titleArg = parser.value(titleOption);
+  const QString iconArg = parser.value(iconOption);
+
+  const QStringList args = parser.positionalArguments();
+  if (args.size() != 1)
+  {
+    qCritical("Error: missing URL");
+    parser.showHelp(1);
+  }
+
+  const QUrl url = args.at(0);
+  if (!url.isValid() || url.scheme().size() == 0)
+  {
+    qCritical("Error: invalid URL format.");
+    exit(1);
+  }
+
   int defaultLoggingMode = LoggingMode::Console | LoggingMode::Journal;
 #ifdef QT_DEBUG
   int defaultLogLevel = LOG_DEBUG;
@@ -92,33 +205,6 @@ int main(int argc, char *argv[])
 #endif
 
   PTLogger::initialiseLogger(defaultLoggingMode, defaultLogLevel);
-
-  QGuiApplication app(argc, argv);
-  QtWebEngine::initialize();
-
-  QCommandLineParser parser;
-  parser.setApplicationDescription(QGuiApplication::applicationDisplayName());
-  QCommandLineOption windowedModeOption(QStringList() << "wm" << "windowed", "windowed mode");
-  parser.addOption(windowedModeOption);
-  QCommandLineOption widthOption(QStringList() << "w" << "window-width", "window width relative to screen", "width", "0.65");
-  parser.addOption(widthOption);
-  QCommandLineOption heightOption(QStringList() << "h" << "window-height", "window height relative to screen", "height", "0.55");
-  parser.addOption(heightOption);
-
-  parser.process(app);
-
-  bool windowedModeArg = parser.isSet(windowedModeOption);
-
-  bool widthArg;
-  QString widthStr = parser.value(widthOption);
-  float width = widthStr.toFloat(&widthArg);
-
-  bool heightArg;
-  QString heightStr = parser.value(heightOption);
-  float height = heightStr.toFloat(&heightArg);
-
-  QStringList args = app.arguments();
-  qDebug() << args;
 
   QString configFilePath;
   if (isPi())
@@ -182,66 +268,35 @@ int main(int argc, char *argv[])
   ////////////////
   qInfo() << "Configuring view";
 
-  QString title = QStringLiteral("pi-topOS First Time Setup");
-  app.setApplicationName(title);
-  rootObject->setProperty("title", title);
-
-  QString url = "http://localhost:80";
-  rootObject->setProperty("url", url);
+  app.setApplicationName(titleArg);
+  rootObject->setProperty("title", titleArg);
 
   const QSize &screenSize = app.primaryScreen()->size();
-  if (windowedModeArg && widthArg)
-  {
-    rootObject->setProperty("width", width*screenSize.width());
-  }
-  if (windowedModeArg && heightArg)
-  {
-    rootObject->setProperty("height", height*screenSize.height());
-  }
-  if (! windowedModeArg)
+  if (kioskModeArg)
   {
     rootObject->setProperty("visibility", "FullScreen");
     rootObject->setProperty("width", screenSize.width());
     rootObject->setProperty("height", screenSize.height());
   }
+  else
+  {
+    rootObject->setProperty("visibility", "Windowed");
+    rootObject->setProperty("height", height*screenSize.height());
+    rootObject->setProperty("width", width*screenSize.width());
+  }
+  rootObject->setProperty("url", url);
+
+  window->setFlag(Qt::FramelessWindowHint, hideWindowFrameArg);
+
+  if (!iconArg.isEmpty())
+  {
+    const QIcon icon(iconArg);
+    app.setWindowIcon(icon);
+  }
+
   rootObject->setProperty("initialised", true);
 
-  qInfo() << "Waiting for backend web server response...";
-  bool serverIsUp = false;
-  int counter = 0;
-  int counterMax = 30;
-  while (serverIsUp == false)
-  {
-    if (counter >= counterMax)
-    {
-      qFatal("Unable to contact web server!");
-      exit(1);
-    }
-
-    QString resp;
-    int exitCode = runCommand("curl",
-                              QStringList() << "--max-time"
-                                            << "1"
-                                            << "--silent"
-                                            << "--fail"
-                                            << "--output"
-                                            << "/dev/null" << url,
-                              1000, resp);
-
-    qDebug() << exitCode;
-
-    if (exitCode == 0)
-    {
-      qInfo() << "Backend web server responded!";
-      break;
-    }
-    else
-    {
-      qInfo()
-          << "Backend web server did not respond - sleeping for 1 second...";
-      QThread::sleep(1);
-    }
-  }
+  waitForServerResponse(url);
 
   ///////////////
   // MAIN LOOP //
